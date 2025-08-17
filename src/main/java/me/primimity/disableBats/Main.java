f3676b982960757d0aee9d29afc1e9c3ecf1df04
Primimity <primimity@gmail.com>
2025-08-17 12:18:36 -0500

    Initial commit

--- src/main/java/me/primimity/disableBats/Main.java
+++ src/main/java/me/primimity/disableBats/Main.java
@@ -1,17 +1,39 @@
 package me.primimity.disableBats;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Bat;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
 
+        Bukkit.getWorlds().forEach(w -> w.getEntitiesByClass(Bat.class).forEach(Entity::remove));
+
+        System.out.println("\n" +
+                getDescription().getName() + " v" + getDescription().getVersion() + "\n" +
+                "Created by " + getDescription().getAuthors() + "\n" +
+                "Need a custom plugin? Discord me @primimity\n");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    // DisableBats
+    @EventHandler
+    public void onBatSpawn(CreatureSpawnEvent e) {
+        if (e.getEntityType() == EntityType.BAT) {
+            e.setCancelled(true);
+        }
     }
 }
